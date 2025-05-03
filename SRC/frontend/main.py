1b64330b1155d48d70f805af23d1c3357e58453d
Cil4ntr0 <felipesanchezq6@gmail.com>
2025-05-03 14:50:39 -0500

    segarega una nueva pagina gestion de producto

--- SRC/frontend/main.py
+++ SRC/frontend/main.py
@@ -14,7 +14,12 @@ app.mount("/static", StaticFiles(directory=static_path), name="static")
 templates_path = os.path.join(current_dir, "templates")
 templates = Jinja2Templates(directory=templates_path)
 
+# Página principal
 @app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("Principalamd.html", {"request": request})
-    
+
+# Página secundaria: Gestión de personas
+@app.get("/GestionPersona")
+async def gestion_persona(request: Request):
+    return templates.TemplateResponse("GestionPersona.html", {"request": request})
